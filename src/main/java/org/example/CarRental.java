543ae0681598781cb85eaa9119aa821ce918772b
wz <wiktor.zareba@teb.edu.pl>
2025-10-15 10:10:13 +0200

    interface commit

---
+++ src/main/java/org/example/CarRental.java
@@ -0,0 +1,36 @@
+package org.example;
+
+public interface CarRental {
+    /**
+     * Dodaje nowego klienta do systemu.
+     * @return Unikalny identyfikator (ID) nowego klienta.
+     */
+    String addCustomer(String firstName, String lastName, String driverLicenseNumber);
+
+    /**
+     * Usuwa klienta z systemu na podstawie jego ID.
+     */
+    void removeCustomer(String customerId);
+
+    /**
+     * Dodaje nowy samochód do salonu.
+     * @return Unikalny identyfikator (ID) nowego samochodu.
+     */
+    String addCar(String brand, String model, String registrationNumber);
+
+    /**
+     * Usuwa samochód z salonu na podstawie jego ID.
+     */
+    void removeCar(String carId);
+
+    /**
+     * Przypisuje samochód do klienta (wypożycza go).
+     * Na razie nie sprawdzamy, czy samochód jest już wypożyczony (choć powinniśmy!)
+     */
+    void rentCar(String customerId, String carId);
+
+    /**
+     * Zwraca samochód do salonu.
+     */
+    void returnCar(String carId);
+}
